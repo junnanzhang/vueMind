250801830fe5c5bbd5f8e8d485f6b5113a8b9df0
zhangjunnan <zhangjunnan1@jd.com>
2018-12-27 14:40:40 +0800

    add vue test

---
+++ vuetest4.js
@@ -0,0 +1,21 @@
+const nodeOps = {
+    setTextContent (text) {
+        if (platform === 'weex') {
+            node.parentNode.setAttr('value', text);
+        } else if (platform === 'web') {
+            node.textContent = text;
+        }
+    },
+    parentNode () {
+        //......
+    },
+    removeChild () {
+        //......
+    },
+    nextSibling () {
+        //......
+    },
+    insertBefore () {
+        //......
+    }
+}
